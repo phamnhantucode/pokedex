ccef85de31be174dce3dd5feb4d7a27786957807
phamnhantucode <bao.trancong.2003@gmail.com>
2022-09-22 20:38:40 +0700

    inital

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        /* .type-small {
+            line-height: 0.5;
+
+        } */
+        body {
+            background: transparent url("https://i.pinimg.com/236x/62/89/31/6289310c8dff8f609bc7c2699cbea876.jpg");
+            /* background-repeat: no-repeat; */
+            /* background-size: cover; */
+        }
+        .badge-violet {
+            background-color: rgb(201, 85, 255);
+            color: #fff;
+        }
+        
+        .container {
+            border-radius: 15px;
+        }
+        .bg-grass {
+            background-color: #dcffb2 !important;
+        }
+
+    </style>
+  </head>
+  <body>
+    
+    <div class="container">
+        <div class="container p-2 mt-5 bg-grass">
+            <div class="row">
+                <div class="col-4">
+                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""> 
+                </div>
+                <div class="col-8 pt-4 pl-4">
+                    <div class="row">
+                        <h1>Bulbasaur</h1>
+                        <div class="my-auto ml-2">
+                            <a href="#" class="badge badge-success ">Grass</a>
+                            <a href="#" class="badge badge-violet">Posion</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <a href="">Generation 1</a>
+                    </div>
+                    <div class="row">
+                        <div class="level-star">
+                            <img src="https://wiki.hoyolab.com/_nuxt/img/levelStar.2559336.png" alt="">
+                            <img src="https://wiki.hoyolab.com/_nuxt/img/levelStar.2559336.png" alt="">
+                            <img src="https://wiki.hoyolab.com/_nuxt/img/levelStar.2559336.png" alt="">
+                            <img src="https://wiki.hoyolab.com/_nuxt/img/levelStar.2559336.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
